3b088a50d8e85029575a47216cbcd977ff5c1244
thuannm <thuannm.ot@gmail.com>
2025-10-31 09:09:54 +0700

    + Add : check step ( 60%) - Update : SuccessPopup ( tex)

--- Assets/Master/Scripts/Core/StepBase.cs
+++ Assets/Master/Scripts/Core/StepBase.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public abstract class StepBase : MonoBehaviour, IStep
 {
+    [Header("Step ID")]
+    [SerializeField] protected int StepID;
     [SerializeField] protected ArrowDirectionController m_ArrowDirectionObject;
+    
     public bool IsFinished { get; protected set; }
     public event Action<StepBase> OnStepFinished;
 
@@ -14,6 +17,7 @@ public abstract class StepBase : MonoBehaviour, IStep
     public virtual void StartStep()
     {
         IsFinished = false;
+        StepTutorialManager.Instance.GotoState(StepID);
     }
     public virtual void FinishStep()
     {
