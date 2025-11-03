15a5d1563250de997612b6d5f4f658a8022c5c73
thuannm <thuannm.ot@gmail.com>
2025-11-03 15:51:10 +0700

    - Update : Default drop time

--- Assets/Master/Scripts/Object/TutorialSteps/DropController.cs
+++ Assets/Master/Scripts/Object/TutorialSteps/DropController.cs
@@ -8,12 +8,14 @@ public class DropController : StepBase
     [SerializeField] private Transform m_ParentDropPos;
     [SerializeField] GameObject m_DropPrefab;
     [SerializeField] private LevelLiquidControl m_LevelLiquidVaseControl;
-    private float m_TimeToCloneDropPrefab = 5f;
+    private float m_TimeToCloneDropPrefab = 1f;
     private float m_TimeToDone = 15f;
     private Coroutine m_EndPopupCoroutine;
+
+    private void Start() => VolumeSliderController.OnTimeSliderValueChanged += SetDefaultDropTime;
+
     public override void StartStep()
     {
-
         base.StartStep();
         VolumeSliderController.OnTimeSliderValueChanged += UpdateDropTime;
         if (m_ArrowDirectionObject != null && m_TargetPosObject != null) m_ArrowDirectionObject.SetPosArrowDirection(m_TargetPosObject);
@@ -30,6 +32,7 @@ public class DropController : StepBase
     {
         base.ResetStep();
         VolumeSliderController.OnTimeSliderValueChanged -= UpdateDropTime;
+        VolumeSliderController.OnTimeSliderValueChanged -= SetDefaultDropTime;
         CancelInvoke(nameof(DropObject));
         m_LevelLiquidVaseControl.ResetFillLevel();
         if (m_EndPopupCoroutine != null)
@@ -40,9 +43,14 @@ public class DropController : StepBase
     }
     private void UpdateDropTime(float value)
     {
-        m_TimeToCloneDropPrefab = value;
+        SetDefaultDropTime(value);
         CancelInvoke(nameof(DropObject));
         InvokeRepeating(nameof(DropObject), m_TimeToCloneDropPrefab, m_TimeToCloneDropPrefab);
+    }
+
+    private void SetDefaultDropTime(float value)
+    {
+        m_TimeToCloneDropPrefab = value;
     }
     private void DropObject()
     {
